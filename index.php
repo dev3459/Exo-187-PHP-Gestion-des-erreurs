91cc8a239f4fc4a2b865f75ae4f5b1f86d9d0154
dev3459 <test45@outlook.fr>
2021-02-12 15:46:39 +0100

    Exo 187 Fini

--- index.php
+++ index.php
@@ -22,9 +22,10 @@ function divide(int $number, int $by) {
 // TODO Compléter le code en fournissent un OU deux zéro en paramètre de la fonction divide !
 try {
     // Corrigez moi ( si je me trompe ! ).
-    $result = divide();
+    echo divide(0, 0);
     // Afficher la variable $result.
 }
-catch(//TODO Complétez moi !) {
+catch(Exception $e) {
     // TODO afficher le message contenu dans $e.
+    echo $e->getLine();
 }
